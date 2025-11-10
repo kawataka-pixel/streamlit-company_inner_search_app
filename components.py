4218a553006d9f5b93ddba02fae6b5cd7f646a45
kawataka-pixel <kawataka1118@gmail.com>
2025-11-10 15:31:20 +0900

    修正

--- components.py
+++ components.py
@@ -26,7 +26,7 @@ def display_select_mode():
     回答モードのラジオボタンを表示
     """
     # 回答モードを選択する用のラジオボタンを表示
-    col1, col2 = st.columns([100, 1])
+    col1, col2 = st.columns([3, 2])
     with col1:
         # 「label_visibility="collapsed"」とすることで、ラジオボタンを非表示にする
         st.session_state.mode = st.radio(
